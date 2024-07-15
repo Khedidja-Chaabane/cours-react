84882888cdcc6e656c2179ff17834596b1399a2e
Khedidja-Chaabane <khedidjal94@gmail.com>
2024-07-15 23:02:12 +0200

    Todos

--- src/components/DataFetchingTodos.js
+++ src/components/DataFetchingTodos.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
+import Todo from './Todo' // Importation du composant Todo depuis un fichier local
+import './todo.css' // Importation du fichier CSS pour les styles du composant Todo
+
 
 function DataFetchingTodos() {
     const [loading, setLoading] = useState(true);
@@ -21,20 +24,30 @@ function DataFetchingTodos() {
             });
     }, []);
 
-return (
-    <React.Fragment>
-        {loading ? 'Loading .....' : todos.map((todo, index) => {
-            return (
-                <div key={index}>
-                    <h2>titre: {todo.title}</h2>
-                    <p>user id : {todo.userId}</p>
-                    <p>{todo.copleted ? 'Completed' : 'Non completed'}</p>
-                    <hr />
-                </div>
-            )
-        })}
-    </React.Fragment>
-  )
+    return (
+        <React.Fragment>
+            {/* Fragment React utilisé pour grouper plusieurs éléments sans ajouter de nœud supplémentaire au DOM */}
+            {/* // Si le chargement est terminé, mapper sur les todos pour afficher chaque todo */}
+            {loading ? 'Loading .....' : todos.map((todo, index) => {
+                //premiere methode
+                // let theme = todo.completed ? 'green' : 'red'; // Définition de la variable "theme" pour appliquer la classe CSS basée sur l'état "completed" ou non
+
+                return (
+                    //deuxieme methode
+                    // Retourner le composant Todo pour chaque todo
+                    <Todo key={index} todo = {todo}/> // Utilisation du composant Todo avec une prop todo// Le composant Todo est utilisé pour afficher les détails de chaque todo. 
+
+                    //premiere methode : suite
+                    // <div key={index} className={theme}>
+                    //     <h2>titre: {todo.title}</h2>
+                    //     <p>user id : {todo.userId}</p>
+                    //     <p>Statut : {todo.completed ? 'Completed' : 'Non completed'}</p>
+                    //     <hr />
+                    // </div>
+                )
+            })}
+        </React.Fragment>
+    )
 }
 
 export default DataFetchingTodos
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import './todo.css'
-function Todo({todo}) {
-    let theme = todo.completed == true ? 'green': 'red';
+function Todo({ todo }) {
+  let theme = todo.completed ? 'green' : 'red';  // Définition de la variable "theme" qui sera 'green' si todo.completed est vrai, sinon 'red'
+
   return (
-    <div className='theme'>
-        Todo
-        </div>
+    <React.Fragment>
+ {/* Fragment React utilisé pour grouper plusieurs éléments sans ajouter de nœud supplémentaire au DOM */}
+      <div className={theme}> {/* Application de la classe CSS basée sur l'état "completed" du todo */}
+        <h2>Titre : {todo.title}</h2>
+        <p>User id : {todo.userId}</p>
+        <p>Statut : {todo.completed ? 'Completed' : 'Non completed'}</p>
+        <hr/>
+      </div>
+    </React.Fragment>
+
   )
 }
 
